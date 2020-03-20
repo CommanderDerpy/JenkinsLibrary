7962fc2fedfc62c38d3e9b7ad9fd9e116acea623
CommanderDerpy <max.gewers@gmail.com>
2020-03-20 11:19:54 -0600

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -5,3 +5,7 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def warning(message) {
+    booleanParam(name: 'RELEASE', defaultValue: false, description: 'Whether to release the project or not. (Implies version increase) - Default: false.')
+}
